67b1cdc0e3e4b14b63757349204dd302ba3025a2
psp10 <psp10@SHAHAD>
2024-12-17 12:05:09 +0300

    Add project files.

---
+++ ifStatementBMI/Program.cs
@@ -0,0 +1,115 @@
+﻿// Shahad Eissa
+// Calculate BMI:
+Console.WriteLine("enter you weight: ");
+double weight = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter your height in cm: ");
+double height = Convert.ToDouble(Console.ReadLine());
+double heightInMeter = height / 100;
+double BMI = (weight / Math.Pow(heightInMeter,2) );
+string BMIState;
+Console.WriteLine("BMI is: " + BMI);
+
+// BMI state:
+if (BMI < 18.5)
+{
+    BMIState = ("under weight");
+    Console.WriteLine(BMIState);
+}
+else if (BMI >= 18.5 && BMI <= 24.9)
+{
+    BMIState = ("healthy");
+    Console.WriteLine(BMIState);
+}
+else if (BMI >= 25 && BMI <= 29.9)
+{
+    BMIState = ("over weight");
+    Console.WriteLine(BMIState);
+} 
+else if (BMI >= 18.5 && BMI <= 24.9)
+{
+    BMIState = ("obese");
+    Console.WriteLine(BMIState);
+}
+
+// BMI state by age
+Console.WriteLine("enter your age");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 19 && age <= 24)
+{
+    if (BMI >= 19 && BMI <= 24)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
+else if (age >= 25 && age <= 34)
+{
+    if (BMI >= 20 && BMI <= 25)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
+else if (age >= 35 && age <= 44)
+{
+    if (BMI >= 21 && BMI <= 26)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
+else if (age >= 45 && age <= 54)
+{
+    if (BMI >= 22 && BMI <= 27)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
+else if (age >= 55 && age <= 64)
+{
+    if (BMI >= 23 && BMI <= 28)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
+else if (age >= 65)
+{
+    if (BMI >= 24 && BMI <= 29)
+    {
+        BMIState = ("healthy");
+        Console.WriteLine(BMIState);
+    }
+    else
+    {
+        BMIState = ("not healthy");
+        Console.WriteLine(BMIState);
+    }
+}
